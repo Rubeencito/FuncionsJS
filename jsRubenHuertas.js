8755f1225b8d8322101d30dd900f93bf107e9c6c
Rubeencito <ruben.huertas@institutvidreres.cat>
2021-11-30 08:26:09 +0100

    EX10

--- jsRubenHuertas.js
+++ jsRubenHuertas.js
@@ -1,8 +1,19 @@
-//EX9
-var num = function (numero1, numero2) {
-    if (numero1 == 30 || numero2 == 30 || numero1+numero2 == 30) {
+//EX10
+
+var num = (numero1,numero2) => {
+    if (numero1===numero2)
     return true
-    }
-}  
-console.log(num(15,15))
+    else return false
+}
+console. log(num(7,7))
+
+//EX10
+
+var num = (a, b) => {
+    if (a===b)
+    return true
+    else return false
+}
+console. log(num(17, 7))
     
+
